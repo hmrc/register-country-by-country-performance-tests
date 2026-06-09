ab9ae00432f9e962f7d05eff8b4fa3529ab9184c
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2026-06-09 14:06:26 +0100

    chore: upgrade to Scala 3, Java 21, and update sbt plugins

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,8 @@ import sbt._
 
 object Dependencies {
 
-  private val gatlingVersion = "3.4.2"
-
   val test = Seq(
-    "com.typesafe" % "config"                  % "1.3.1" % Test,
+    "com.typesafe" % "config"                  % "1.4.9" % Test,
     "uk.gov.hmrc" %% "performance-test-runner" % "6.3.0" % Test
   )
 }
